05918d3f58424ca2c2a38eaa507d03be6c3c03c5
Yiğit Özyılmaz <keremyigit1995@gmail.com>
2024-06-18 22:45:45 +0300

    mini update

--- app/page.js
+++ app/page.js
@@ -400,16 +400,15 @@ export default function Page() {
                 FREQUENTLY ASKED QUESTIONS
               </h1>
               <div className='flex flex-col gap-4 max-w-[800px] mt-24 w-full h-full px-8'>
-                <FaQ q='What is the Evil Wizards NFT Collection?' a='The Evil Wizards NFT Collection is part of a new brand called Evil Magic, which is heavily focused on creating a culture and intellectual property (IP). The NFT collection serves as a premium subscription model that connects the brands by-products, including games and other content, offering users exclusive privileges in both digital and physical realms. Additionally, it aims to create an inner circle for the brand itself.' />
-                <FaQ q='What are the Benefits of Our NFTs?' a={` Revenue Sharing: Be part of the revenue sharing model in future product releases.
-      Pre-Mint Whitelist: Automatic whitelist opportunities for future collections.
+                <FaQ q='What is the Collection?' a='The Collection is part of a new brand called Evil Magic, which is heavily focused on creating a culture and intellectual property (IP). The NFT collection serves as a premium subscription model that connects the brands by-products, including games and other content, offering users exclusive privileges in both digital and physical realms. Additionally, it aims to create an inner circle for the brand itself.' />
+                <FaQ q=' What Benefits Do Our NFTs Offer?' a={` Revenue Sharing: Be part of the revenue sharing model in future product releases.
       Event Entries: Access to exclusive events.
       Priority and various advantages in upcoming product releases.
       VIP Membership: Become a VIP member of the Evil Magic community and an alpha tester.
       Design Participation: Get involved in the design processes of the EM world.
-      Exclusive Collectibles: Special physical collectibles for top buyers.`} />
-                <FaQ q='What is the Release Date and Price of the Evil Wizards NFT Collection?' a='The release date and price of the Evil Wizards NFT Collection are yet to be announced. Stay tuned for updates. (TBA - To Be Announced)' />
-                <FaQ q='How Many Pieces Will the Evil Wizards NFT Collection Include?' a='The total number of pieces in the collection has not been determined yet. Details will be announced soon. (TBA - To Be Announced)' />
+      Exclusive Collectibles: Special physical collectibles for top community members.`} />
+                <FaQ q='What is the Release Date and Price of the Collection?' a='The release date and price of the Collection are yet to be announced. Stay tuned for updates. (TBA - To Be Announced)' />
+                <FaQ q='How Many Pieces Will the Collection Include?' a=' Details will be announced soon. (TBA - To Be Announced)' />
               </div>
               <div className='relative flex items-center justify-center w-full mt-24'>
                 <div><Image src='/Group.png' width='1500' height='500' className='object-contain z-[0] p-4 relative opacity-[0,8] ' style={{ marginBottom: '-92px' }} /></div>
@@ -593,16 +592,15 @@ export default function Page() {
               </h1>
 
                 <div className='flex flex-col gap-4 max-w-[800px] mt-24 w-full h-full px-8'>
-                  <FaQ q='What is the Evil Wizards NFT Collection?' a='The Evil Wizards NFT Collection is part of a new brand called Evil Magic, which is heavily focused on creating a culture and intellectual property (IP). The NFT collection serves as a premium subscription model that connects the brands by-products, including games and other content, offering users exclusive privileges in both digital and physical realms. Additionally, it aims to create an inner circle for the brand itself.' />
-                  <FaQ q='What are the Benefits of Our NFTs?' a={` Revenue Sharing: Be part of the revenue sharing model in future product releases.
-      Pre-Mint Whitelist: Automatic whitelist opportunities for future collections.
+                  <FaQ q='What is the Collection?' a='The Collection is part of a new brand called Evil Magic, which is heavily focused on creating a culture and intellectual property (IP). The NFT collection serves as a premium subscription model that connects the brands by-products, including games and other content, offering users exclusive privileges in both digital and physical realms. Additionally, it aims to create an inner circle for the brand itself.' />
+                  <FaQ q='What Benefits Do Our NFTs Offer?' a={` Revenue Sharing: Be part of the revenue sharing model in future product releases.
       Event Entries: Access to exclusive events.
       Priority and various advantages in upcoming product releases.
       VIP Membership: Become a VIP member of the Evil Magic community and an alpha tester.
       Design Participation: Get involved in the design processes of the EM world.
-      Exclusive Collectibles: Special physical collectibles for top buyers.`} />
-                  <FaQ q='What is the Release Date and Price of the Evil Wizards NFT Collection?' a='The release date and price of the Evil Wizards NFT Collection are yet to be announced. Stay tuned for updates. (TBA - To Be Announced)' />
-                  <FaQ q='How Many Pieces Will the Evil Wizards NFT Collection Include?' a='The total number of pieces in the collection has not been determined yet. Details will be announced soon. (TBA - To Be Announced)' />
+      Exclusive Collectibles: Special physical collectibles for community members.`} />
+                  <FaQ q='What is the Release Date and Price of the Collection?' a='The release date and price of the Collection are yet to be announced. Stay tuned for updates. (TBA - To Be Announced)' />
+                  <FaQ q='How Many Pieces Will the Collection Include?' a=' Details will be announced soon. (TBA - To Be Announced)' />
                 </div>
               </div>
               <div className='relative flex items-center justify-center w-full mt-20'>
