a0978fdb36d976a6dbf1a32e8643abad76907743
Yiğit Özyılmaz <keremyigit1995@gmail.com>
2024-10-26 20:45:04 +0300

    ddd

--- components/altasker.jsx
+++ components/altasker.jsx
@@ -22,8 +22,7 @@ const MyComponent = () => {
               alt="CTU Logo" 
               width="700" 
               height="500" 
-              className="object-contain z-[12] relative opacity-[1] mr-10  mb-[-500px] md:top-[-420px] 2xl:top-[-420px]"
-
+              className=" z-[12] relative opacity-[1] mr-10 sm:w-[700px] 2xl:w-[700px] mb-[-500px] md:top-[-410px] 2xl:top-[-420px]"
             />
           </a>
 
